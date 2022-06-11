069b3fd9d891088c12d16a7a8362626c3357af12
mr-pmillz <>
2022-06-11 02:09:03 -0400

    updated comment

--- plugin/exec_aquatone.sh
+++ plugin/exec_aquatone.sh
@@ -4,10 +4,11 @@
 #  Filename: Sudomy - Subdomain Enumeration & Analysis        #
 #-Author(s)---------------------------------------------------#
 #  Edo maland ~ @screetsec                                    #
+#  @mr-pmillz                                                 #
 #-Info--------------------------------------------------------#
 #  This file is part of Sudomy project                        #
-#  Plugin Screenshots: Update = 2020-06-26                    #
-#	- gowitness					      #
+#  Plugin Screenshots: Update = 2022-06-11                    #
+#	- aquatone					      #
 #-Licence-----------------------------------------------------#
 #  MIT License ~ http://opensource.org/licenses/MIT           #
 #-------------------------------------------------------------#
@@ -16,7 +17,7 @@ function exec_aquatone(){
 rm -rf ${OUT}/${DATE_LOG}/${DOMAIN}/screenshots/* ## Cleaning old file
 		echo -e "\n${BOLD}[${LGREEN}+${RESET}${BOLD}]${RESET} Web Screenshots: from domain list"
 		echo -e "---------------------------------------------\n"
-		## Check Folder Results	
+		## Check Folder Results
 		[[ ! -e "${OUT}/${DATE_LOG}/${DOMAIN}/screenshots" ]] && mkdir -p "${OUT}/${DATE_LOG}/${DOMAIN}/screenshots" || true
 		cat ${OUT}/${DATE_LOG}/${DOMAIN}/${RESULT_HTTPROBE} | ${_AQUATONE} -ports 80,443 -out ${OUT}/${DATE_LOG}/${DOMAIN}/screenshots -screenshot-timeout 40000
 }
